252c99ee6144f07d8c9b6e556439abe55aa4f87f
Sarthak Sahu <sarsahu@microsoft.com>
2019-12-26 18:46:58 +0530

    add user header component

---
+++ src/components/UserHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class UserHeader extends React.Component {
+    render() {
+        return (
+            <div>
+                User Header
+            </div>
+        );
+    }
+}
