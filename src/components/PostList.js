a48c560f222f036160679edef61d4bb12e45290a
Sarthak Sahu <sarsahu@microsoft.com>
2019-12-26 18:59:44 +0530

    add user reducer

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
-import { UserHeader } from './UserHeader';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     componentDidMount() {
